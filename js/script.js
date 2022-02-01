b9a6613945134db5d9faaefc2496c6b129829124
Ira Evers <iraevers34@gmail.com>
2022-02-01 20:12:32 +0100

    Commit 0.3
    Updating Project page.
    Adding images and image structure.
    Java scripting Alerts.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,24 @@ $(document).ready(documentReady);
 
 function documentReady() {
     $('.Arrowup').click(Scrollup);
+    $('.projects a').click(UnderConstruction);
+    $('.btnResume').click(ResumeUpdated);
 }
 
 function Scrollup(event) {
     event.preventDefault();
     console.log("Go to top button is pressed" + $(window).scrollTop());
     $(window).scrollTop(0);
+}
+
+function UnderConstruction(event) {
+    event.preventDefault();
+    console.log("Alert UnderConstruction");
+    alert("My apoligies. The detail page of the project is underconstruction.");
+}
+
+function ResumeUpdated(event) {
+    event.preventDefault();
+    console.log("Alert Resume being updated");
+    alert("My apoligies. I am updating my resume at the moment.");
 }

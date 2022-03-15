84b1efb567b8f8ebfeb6c27a9bcaec9cd11f1e95
Ira Evers <iraevers34@gmail.com>
2022-03-15 19:05:37 +0100

    Commit 6
    Looked with Vy at the website.
    - Add SPACE project
    - Changed normal font to avernir
    - Changed profile picture
    - Made profile picture javascripting
    - Chnaged profile picture Resume
    - Changed header (spacing)

--- js/script.js
+++ js/script.js
@@ -18,8 +18,19 @@ function UnderConstruction(event) {
     alert("My apoligies. The detail page of the project is underconstruction.");
 }
 
-// function ResumeUpdated(event) {
-//     event.preventDefault();
-//     console.log("Alert Resume being updated");
-//     alert("My apoligies. I am updating my resume at the moment.");
-// }
+function ResumeUpdated(event) {
+    event.preventDefault();
+    console.log("Alert Resume being updated");
+    alert("My apoligies. I am updating my resume at the moment.");
+}
+
+
+function ChangeProfileBW() {
+    document.getElementById("iraAboutpic").src = iraAboutpic.src.replace("Ira_RGB_Final.png", "Ira_BW.png");
+    console.log("Profile Image changed");
+}
+
+function ChangeProfileColor() {
+    document.getElementById("iraAboutpic").src = iraAboutpic.src.replace("Ira_BW.png", "Ira_RGB_Final.png", );
+    console.log("Profile Image changed back");
+}
